b1e5e7134d2e723d2801b1053a3213ea5c93b16c
Regan Barua <reganbarua2015@gmail.com>
2022-06-04 13:37:19 -0700

    multiple component added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import "./App.css";
+import Home from "./Home";
+import Navbar from "./Navbar";
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  // const person = { name: "yoshi", age: 30 };
-  const link = "http://www.google.com";
+  // const title = "Welcome to the new blog";
+  // const likes = 50;
+  // // const person = { name: "yoshi", age: 30 };
+  // const link = "http://www.google.com";
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
+        <Home />
+        {/* <h1>{title}</h1>
         <p>Liked {likes} times</p>
-        {/* <p>{person}</p> */}
+         <p>{person}</p> 
         <p>{10}</p>
         <p>{"hello,ninjas"}</p>
         <p>{[1, 2, 3, 4, 5]}</p>
         <p>{Math.random() * 10}</p>
-        <a href={link}>Google site</a>
+        <a href={link}>Google site</a>  */}
       </div>
     </div>
   );
